96ca5f8c7a9178c4c0111271051c5ae2ebb81d84
mevbot1256 <mevbot1256@proton.me>
2024-06-02 14:16:02 -0700

    Add files via upload

---
+++ addressnewaccount.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Ethereum: 0xda18329F047a0D782ca868d515728c964F294349 > crypto_addresses.txt
+echo Solana: 97cfqN15hbwCn9VGRRyBpEiQBtwiRLm24AXrAVGVoLxq >> crypto_addresses.txt
+echo Polygon (Matic): 0xda18329F047a0D782ca868d515728c964F294349 >> crypto_addresses.txt
+echo File created successfully.
+pause
